cf5314baffe6d969a72e879f1cc980e55e001835
Ashley Grealish <grealish10@hotmail.com>
2013-03-29 00:21:53 +0000

    ADC now read Continuously with DMA Access
    Should reduce CPU load fix the ADC not reading error.
    
    May need to add low pass filtering with 20kHz cut off to remove
    switching noise. Simply averaging many reads of ADC will no longer work.

--- Software/standalone/adc.c
+++ Software/standalone/adc.c
@@ -1,36 +1,23 @@
 #include "adc.h"
 
-// TODO: this need to select channel rather than the ADC, there is only 1 ADC
-// with 10 channels on the target chip (Discovery board has 16 channels)
+#define ADC1_DR_Address 0x40012440
+
+
+volatile uint16_t RegularConvData_Tab[2];
+
+
 float get_adc_voltage ( uint32_t ADC_Channel )
 {
-	uint16_t ADCConvertedValue = 0;
-	float ADCConvertedVoltage = 0;
 	
-	init_adc(ADC_Channel);
-	
-	//Set up reading from the correct Channel, 28 cycle sampling time
-	ADC_ChannelConfig(ADC1, ADC_Channel , ADC_SampleTime_239_5Cycles);
-	
-	 /* Enable ADCperipheral[PerIdx] */
-  ADC_Cmd(ADC1, ENABLE);     
-  
-  /* Wait the ADCEN falg */
-  while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_ADEN))
-		os_tsk_pass(); 
-	
-	//Start the convertions and wait for it to complete
-	ADC_StartOfConversion(ADC1);
-	while(ADC_GetFlagStatus(ADC1, ADC_FLAG_EOC) == RESET)
-		os_tsk_pass();
-	
-	// Get ADC converted data 
-	ADCConvertedValue = ADC_GetConversionValue(ADC1);
-	
-	// Compute the voltage 
-	ADCConvertedVoltage = (ADCConvertedValue * ADC_VREF) / 0xFFF;
-	
-	return ADCConvertedVoltage;
+	switch (ADC_Channel)
+	{
+		case ADC_Channel_10:
+			return ( (RegularConvData_Tab[0] * ADC_VREF) / 0xFFF );
+		case ADC_Channel_11:
+			return ( (RegularConvData_Tab[1] * ADC_VREF) / 0xFFF );
+		default:
+			return -1.0f;
+	}
 	
 }
 
@@ -46,22 +33,20 @@ __task void adc_in (void)
 	}
 }
 
-/**
-  * @brief  ADC and TIM configuration
-  * @param  None
-  * @retval None
-  */
+//
+//  * @brief  ADC configuration
+//  * @param  None
+//  * @retval None
+   
 void init_adc(uint32_t ADC_Channel)
 {
-	ADC_InitTypeDef          ADC_InitStructure;
-	GPIO_InitTypeDef         GPIO_InitStructure;
+	ADC_InitTypeDef ADC_InitStructure;
+	DMA_InitTypeDef DMA_InitStructure;
+	GPIO_InitTypeDef GPIO_InitStructure;
 		
   // GPIOC Periph clock enable 
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-	
-  // ADC1 and TIM1 Periph clock enable 
-  RCC_APB2PeriphClockCmd( RCC_APB2Periph_ADC1 , ENABLE);
-	
+
   
 	GPIO_StructInit(&GPIO_InitStructure);
   // Configure ADC Channel11 PC1 as analog input 
@@ -72,33 +57,66 @@ void init_adc(uint32_t ADC_Channel)
 	// Configure ADC Channel10 PC0 as analog input 
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 ;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
-	
-  // ADCs DeInit 
+  
+  // ADC1 DeInit    
   ADC_DeInit(ADC1);
   
-  // Initialize ADC structure 
+  // ADC1 Periph clock enable  
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+  
+  // DMA1 clock enable  
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1 , ENABLE);
+  
+  // DMA1 Channel1 Config  
+  DMA_DeInit(DMA1_Channel1);
+  DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)ADC1_DR_Address;
+  DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)RegularConvData_Tab;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+  DMA_InitStructure.DMA_BufferSize = 2;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_HalfWord;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Circular;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+  DMA_Init(DMA1_Channel1, &DMA_InitStructure);
+  
+  // DMA1 Channel1 enable  
+  DMA_Cmd(DMA1_Channel1, ENABLE);
+  
+  // ADC DMA request in circular mode  
+  ADC_DMARequestModeConfig(ADC1, ADC_DMAMode_Circular);
+  
+  // Enable ADC_DMA  
+  ADC_DMACmd(ADC1, ENABLE);  
+  
+  // Initialize ADC structure  
   ADC_StructInit(&ADC_InitStructure);
   
-  // Configure the ADC1 in continous mode withe a resolutuion equal to 12 bits  
+  // Configure the ADC1 in continous mode withe a resolutuion equal to 12 bits   
   ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
-  ADC_InitStructure.ADC_ContinuousConvMode = DISABLE;//ENABLE; 
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE; 
   ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
-	ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
-  ADC_InitStructure.ADC_ScanDirection = ADC_ScanDirection_Upward;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_ScanDirection = ADC_ScanDirection_Backward;
   ADC_Init(ADC1, &ADC_InitStructure); 
+ 
+  // Convert the ADC1 Channel 10  with 239 Cycles as sampling time   
+  ADC_ChannelConfig(ADC1, ADC_Channel_10 , ADC_SampleTime_239_5Cycles);
   
-  /* Convert the ADC1 Channel 11 with 28 Cycles as sampling time */ 
-  ADC_ChannelConfig(ADC1, ADC_Channel, ADC_SampleTime_239_5Cycles);
+  // Convert the ADC1 Channel 11  with 239 Cycles as sampling time   
+  ADC_ChannelConfig(ADC1, ADC_Channel_11 , ADC_SampleTime_239_5Cycles);
   
-  /* ADC Calibration */
+  // ADC Calibration  
   ADC_GetCalibrationFactor(ADC1);
   
-  /* Enable ADCperipheral[PerIdx] */
+  // Enable ADC1  
   ADC_Cmd(ADC1, ENABLE);     
   
-  /* Wait the ADCEN falg */
+  // Wait the ADCEN falg  
   while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_ADEN)); 
   
-  /* ADC1 regular Software Start Conv */ 
+  // ADC1 regular Software Start Conv   
   ADC_StartOfConversion(ADC1);
 }
